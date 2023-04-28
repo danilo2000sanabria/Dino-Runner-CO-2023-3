1953be5721cab5bc24aeebe94f3bbda6dd50a1b2
Oscardev <oscar.sanabria@fundacion-jala.org>
2023-04-27 22:07:45 -0500

    CLASE 1

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,14 @@
+from dino_runner.utils.constants import JUMPING, RUNNING, DUCKING
+
+class Dinosaur:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def duck(self):
+        pass
+
+    def jump(self):
+        pass
---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,5 @@
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+class Cactus(Obstacle):
+    def __init__(self):
+        super().__init__()
